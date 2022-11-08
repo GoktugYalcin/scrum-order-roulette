2624d96ff139b9ce243b57e059e5b4b4114837b8
goktug.yalcin <goktug.yalcin@next4biz.com>
2022-11-08 11:46:20 +0300

    History Component integrated with logic

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React from 'react'
+import EmptyState from '../emptyHistory.svg'
+import {RouletteData} from "../redux/slices/rouletteSlice";
+import {useAppSelector} from "../redux/hooks";
 
 const History = () => {
-    return <></>
+    const history: RouletteData[] = useAppSelector(store => store.roulette.historyValue)
+    return <>
+        <div className="roulette__history">
+            <div className="roulette__history-header">
+                History
+            </div>
+            <div className="roulette__history-container">
+                {
+                    history.length ? history.map((step: RouletteData, index: number) => {
+                        return <div className="roulette__history-container_item" key={index}>
+                            {`${index+1}. ${step.option}`}
+                        </div>
+                    }) : <div className="roulette__history-container_empty">
+                        <div className="roulette__history-container_empty-container">
+                            <img src={EmptyState} alt={"History is empty."}/>
+                            No one has selected on roulette.
+                        </div>
+                    </div>
+                }
+            </div>
+        </div>
+    </>
 }
+
+export default History
